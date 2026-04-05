54e2d385a628dec7ede90a17cd1c7789341c404a
mohamedfawzizaki <mohamedfawzizaki@gmail.com>
2026-04-05 16:11:46 +0200

    sddsxcxcxxxxxxxxxxxxxxxx

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from ArgoCD App 6');
+  res.send('Hello from ArgoCD App 7');
 });
 
 app.listen(3000, () => {
